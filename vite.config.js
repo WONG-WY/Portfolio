36a179bcede9ab9ad759468a48fefd163252d7f9
WONG-WY <hkb02u21@gmail.com>
2026-04-20 10:05:44 +0800

    Update html page

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,24 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path' // 必須引入這個來處理路徑
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        'decision-support': resolve(__dirname, 'project-decision-support.html'),
+        'painting-app': resolve(__dirname, 'project-painting-app.html'),
+        'privacy-gateway': resolve(__dirname, 'project-privacy-gateway.html'),
+      },
+    },
+  },
 })
